ddf116de47b19385029a111d2893649d8fd734ca
Eli Elrom <elad.ny@gmail.com>
2020-08-31 10:59:57 +0300

    refactor login

--- react-typescript-redux-login/src/redux/store.ts
+++ react-typescript-redux-login/src/redux/store.ts
@@ -1,10 +1,8 @@
 import { configureStore, combineReducers } from '@reduxjs/toolkit'
-import authReducer from '../features/Authentication/authenticationSlice'
 import notificationSlice from '../features/Notification/notificationSlice'
 
 const store = configureStore({
   reducer: combineReducers({
-    authentication: authReducer,
     notification: notificationSlice,
   }),
 })
